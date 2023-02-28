62beff7de5b99a5aef32c066b1923b83ea1e9e78
Master Yoda Rossi <saulohr@yahoo.com.br>
2023-02-28 20:18:24 -0300

    Renaming and merging

---
+++ 3.py
@@ -0,0 +1,10 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+s = n1+n2
+print("A soma vale entre os números digitados é:",s)
+print(f"A soma vale entre os números digitados é: {n1 + n2}")
+
+# int significa número inteiro"
+# float são números reais, exemplos: 4.5, 7.0, 4.567"
+# bool - booleanos são valores lógicos: True, False"
+# str - string são palavras, que ficam entre aspas"
--- ex003.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número: "))
-s = n1+n2
-print("A soma vale entre os números digitados é: ",s)
-
-"int significa número inteiro"
-"float são números reais, exemplos: 4.5, 7.0, 4.567"
-"bool - booleanos são valores lógicos: True, False"
-"str - string são palavras, que ficam entre aspas"
