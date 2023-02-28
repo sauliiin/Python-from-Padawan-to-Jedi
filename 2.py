b04451019c926c84803d38cb30a9e9bb4670a88d
Master Yoda Rossi <saulohr@yahoo.com.br>
2023-02-28 20:12:41 -0300

    Updating

---
+++ 2.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome:")
+print("É um prazer te conhecer,", nome,"!")
+print("É um prazer te conhecer, {}!".format(nome))
+print(f"É um prazer te conhecer, {nome}!")
--- ex002b.py
+++
@@ -1,4 +0,0 @@
-nome = input("Digite seu nome: ")
-print("É um prazer te conhecer, {}!".format(nome))
-
-#"O que for especificado no .format(), vai ficar dentro do {}"
